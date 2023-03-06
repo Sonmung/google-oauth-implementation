92a0ab6b343da984c3db7aa72d13a4101886c78d
Sonmung <sonmungwork@gmail.com>
2023-03-06 14:49:11 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+
+import jwtDecode from 'jwt-decode';
+import { useState } from 'react';
+import { useGoogleOneTapLogin, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 function App() {
+  const [ user, setUser ] = useState({
+    name:"",
+    email: "",
+    avatarUrl: ""
+  })
+  useGoogleOneTapLogin({
+    onSuccess: credentialResponse => handleSuccess({credentialResponse, type: 'oneTap'}),
+  })
+  const login = useGoogleLogin({
+    onSuccess: tokenResponse => handleSuccess({tokenResponse, type: 'oneClick'}),
+  });
+  async function handleSuccess(data){
+    switch(data.type){
+      case 'oneTap':
+        console.log(data.credentialResponse)
+        const decoded = await jwtDecode(data.credentialResponse.credential)
+        console.log(decoded)
+        setUser({name: decoded.given_name, email: decoded.email, avatarUrl: decoded.picture})
+        break
+      case 'oneClick':
+        try {
+          console.log(data.tokenResponse)
+          let res = await axios(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${data.tokenResponse.access_token}`)
+          console.log(res)
+          setUser({name: res.data.given_name, email: res.data.email, avatarUrl: res.data.picture})
+        } catch (err) {
+          console.log(err.message)
+        }
+        break
+      default:
+        return null
+    }
+    
+  }
+  // function handleLogout(){
+  //   googleLogout();
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <button
+              onClick={login}
+            >
+              Sign in with Google 🚀
+            </button>
+            {/* <GoogleLogin
+              onSuccess={credentialResponse => {
+                console.log({credentialResponse});
+                const decoded = jwtDecode(credentialResponse.credential)
+                console.log(decoded);
+                handleSuccess(decoded)
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+              useOneTap
+            /> */}
+
+            {/* <button onClick={() => login()}>
+              Sign in with Google 🚀{' '}
+            </button>; */}
+            <div>
+              <br></br>
+              {user.avatarUrl && <img src={user.avatarUrl} alt="avatar"></img>}
+              <h1>{user.name}</h1>
+              <p>{user.email}</p>
+            </div>
+            {/* <p>
+              {user.email && <button onClick={handleLogout}>
+                Log Out
+              </button>}
+            </p> */}
     </div>
   );
 }
